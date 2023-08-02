7a610ebd09c1886d78f4a239af7f6f5a947a4e13
Abe Megahed <abem@tds.net>
2023-08-02 09:57:42 -0500

    separated definition/application of control materials

--- css/build.sh
+++ css/build.sh
@@ -1,2 +1,5 @@
 sass specular.scss ../dist/specular.css
-# cssmin ../dist/specular.css > ../dist/specular.css
+	cssmin ../dist/specular.css > ../dist/specular.min.css
+	cp ../dist/specular.css ../demos/demo1/css/specular.css
+	cp ../dist/specular.css ../demos/demo2/css/specular.css
+	cp ../dist/specular.css ../demos/demo3/css/specular.css
